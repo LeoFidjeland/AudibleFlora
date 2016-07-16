af543ded97b3cbf46c953903425af751c6f620ae
Leo Fidjeland <leo.fidjeland@gmail.com>
2016-07-16 17:21:17 +0200

    Twitching

--- flora/flora.ino
+++ flora/flora.ino
@@ -34,31 +34,49 @@
 #define triggerDelayTime 64 // Ultrasound trigger pulse length
 #define measurementPeriod 200 //ms between each ultrasound measurement
 
-// Variables used by program *don't modify*
-volatile double cm = 0.0;
-long startTime = 0;
-long endTime = 0;
-double lastCm = 0.0;
-float playspeedmod = 0;
-float speedchange = 0;
-
 // Objects
 StateVariable <LOWPASS> svf; //möjliga filtertyper: LOWPASS/HIGHPASS/BANDPASS/NOTCH
 Sample <kluck_NUM_CELLS, AUDIO_RATE> aSample(kluck_DATA);
 EventDelay randomDelay;
 EventDelay measurementDelay;
 EventDelay triggerDelay;
+EventDelay twitchDelay;
+EventDelay twitchTimer;
 
 // Sound constants, change these to modify sound behaviour
-const float playspeed = 2.0;
-#define randomPeriod 1000 // ms per random period
+const float basePlayspeed = 30.0;
+#define baseRandomPeriod 200 // ms per random period
+#define randomPeriodRange 100
 #define lowPassResonance 200 // Amount of resonance, between 1 and 255, where 1 is max and 255 min.
-#define lowPassCutoff 400 // Cut off frequency for low pass filter
-#define speedChangeFactor 1.0
+#define lowPassCutoff 3000 // Cut off frequency for low pass filter
+#define speedChangeFactor 5.0
 #define measureFactor 10
+#define twitchLength 300
+#define twitchFactor 10.0
+#define twitchEvaluationRate 100 //ms evaluate if new twich
+#define twitchProbability 3 // 1 / 10
+int twitching = 0;
+
+// Happy and talkative
+//baseRandomPeriod 1000
+//randomPeriodRange 0
+//playspeed 2.0
+//speedChangeFactor 1.0
+//measureFactor 10
+//randomPeriod 1000
+//lowPassCutoff 400
+
+// Variables used by program *don't modify*
+volatile double cm = 0.0;
+long startTime = 0;
+long endTime = 0;
+double lastCm = 0.0;
+float playspeed = 0;
+float speedchange = 0;
+int randomPeriod = baseRandomPeriod;
 
 void setup(){
-//  Serial.begin(9600);
+  Serial.begin(9600);
 
   // Setup ultrasound ISR
   pinMode(trigPin,OUTPUT);
@@ -72,6 +90,8 @@ void setup(){
   aSample.setFreq(playspeed*((float) kluck_SAMPLERATE / (float) kluck_NUM_CELLS));
   aSample.setLoopingOn();
   randomDelay.set(randomPeriod); // 1500 msec countdown, within resolution of CONTROL_RATE
+  twitchDelay.set(twitchEvaluationRate);
+  twitchTimer.set(twitchLength);
   
   svf.setResonance(lowPassResonance);
   svf.setCentreFreq(lowPassCutoff);
@@ -86,7 +106,10 @@ void chooseSpeedMod(){
   }
   cmChange = cmChange / measureFactor;
 //  Serial.println(cmChange);
-  playspeedmod = playspeed + cmChange;
+  playspeed = basePlayspeed + cmChange;
+  if(twitching){
+    playspeed = playspeed + twitchFactor;
+  }
   speedchange = (float)rand((char)-100,(char)100)/800 * speedChangeFactor;
 }
 
@@ -96,6 +119,9 @@ void updateControl(){
   if(randomDelay.ready()){
     // Initiate a new random period for audio
     chooseSpeedMod();
+    randomPeriod = baseRandomPeriod + rand((char)-randomPeriodRange,(char)randomPeriodRange);
+//    Serial.println(randomPeriod);
+    randomDelay.set(randomPeriod);
     randomDelay.start();
   }else if (measurementDelay.ready()){
     // Initiate a new ultrasound measurement by sending a pulse on the trig pin
@@ -107,10 +133,31 @@ void updateControl(){
     measurementDelay.start();
   }else if (triggerDelay.ready()){
     digitalWrite(trigPin, LOW);
+  }else if(twitchDelay.ready()){
+    if(rand((char)twitchProbability) == 0){
+      twitching = 1;
+      Serial.println("Start Twitch");
+      playspeed = playspeed + twitchFactor;
+      twitchTimer.start();
+    }
+    twitchDelay.start();
   }
-  playspeedmod += speedchange;
-//  Serial.println(playspeedmod);
-  aSample.setFreq(playspeedmod);
+
+  if(twitching && twitchTimer.ready()){
+    Serial.println("End Twitch");
+    playspeed = playspeed - twitchFactor;
+    twitching = 0;
+  }
+  
+  
+  playspeed += speedchange;
+  
+//  if(twitching){
+//    playspeedmod = playspeedmod + twitchFactor;
+//  }
+  
+  Serial.println(playspeed);
+  aSample.setFreq(playspeed);
   
 }
 
